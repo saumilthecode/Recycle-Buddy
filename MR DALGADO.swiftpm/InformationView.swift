aadc24bd93f3afc8a232eac159e59c910a7dd313
potato dog man <saumil707@gmail.com>
2024-06-30 22:49:53 +0800

    FIX ALL NAVIGATION LINKS

--- MR DALGADO.swiftpm/InformationView.swift
+++ MR DALGADO.swiftpm/InformationView.swift
@@ -46,13 +46,16 @@ struct InformationView: View {
                             Image("recycle2") // Replace with actual image names
                                 .resizable()
                                 .frame(width: 50, height: 50)
-                            Text("HDPE")
-                                .font(.title2)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color(red: 154/255, green: 199/255, blue: 116/255))
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
+                            NavigationLink(destination: rec2()) {
+                                
+                                Text("HDPE")
+                                    .font(.title2)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color(red: 154/255, green: 199/255, blue: 116/255))
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                            }
                         }
                         
                         Spacer()
@@ -61,13 +64,16 @@ struct InformationView: View {
                             Image("recycle3") // Replace with actual image names
                                 .resizable()
                                 .frame(width: 50, height: 50)
-                            Text("V")
-                                .font(.title2)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color(red: 154/255, green: 199/255, blue: 116/255))
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
+                            NavigationLink(destination: rec3()) {
+                                
+                                Text("V")
+                                    .font(.title2)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color(red: 154/255, green: 199/255, blue: 116/255))
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                            }
                         }
                         Spacer()
 
@@ -76,13 +82,16 @@ struct InformationView: View {
                             Image("recycle4") // Replace with actual image names
                                 .resizable()
                                 .frame(width: 50, height: 50)
-                            Text("LDPE")
-                                .font(.title2)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color(red: 154/255, green: 199/255, blue: 116/255))
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
+                            NavigationLink(destination: rec4()) {
+                                
+                                Text("LDPE")
+                                    .font(.title2)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color(red: 154/255, green: 199/255, blue: 116/255))
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                            }
                         }
                         Spacer()
 
@@ -91,13 +100,16 @@ struct InformationView: View {
                             Image("recycle5") // Replace with actual image names
                                 .resizable()
                                 .frame(width: 50, height: 50)
-                            Text("PP")
-                                .font(.title2)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color(red: 154/255, green: 199/255, blue: 116/255))
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
+                            NavigationLink(destination: rec5()) {
+                                
+                                Text("PP")
+                                    .font(.title2)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color(red: 154/255, green: 199/255, blue: 116/255))
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                            }
                         }
                         Spacer()
 
@@ -105,13 +117,16 @@ struct InformationView: View {
                             Image("recycle6") // Replace with actual image names
                                 .resizable()
                                 .frame(width: 50, height: 50)
-                            Text("PS")
-                                .font(.title2)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color(red: 154/255, green: 199/255, blue: 116/255))
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
+                            NavigationLink(destination: rec6()) {
+                                
+                                Text("PS")
+                                    .font(.title2)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color(red: 154/255, green: 199/255, blue: 116/255))
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                            }
                         }
                         Spacer()
 
@@ -119,13 +134,16 @@ struct InformationView: View {
                             Image("recycle7") // Replace with actual image names
                                 .resizable()
                                 .frame(width: 50, height: 50)
-                            Text("OTHER")
-                                .font(.title2)
-                                .padding()
-                                .frame(maxWidth: .infinity)
-                                .background(Color(red: 154/255, green: 199/255, blue: 116/255))
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
+                            NavigationLink(destination: rec7()) {
+                                
+                                Text("OTHER")
+                                    .font(.title2)
+                                    .padding()
+                                    .frame(maxWidth: .infinity)
+                                    .background(Color(red: 154/255, green: 199/255, blue: 116/255))
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                            }
                         }
                         Spacer()
                         Spacer()
